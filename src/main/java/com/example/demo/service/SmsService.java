9f52753a600d57e837c52c254545fe8c15246f81
levon <levont@energizeglobal.com>
2017-11-29 18:27:22 +0400

    Changes are made doing development

--- src/main/java/com/example/demo/service/SmsService.java
+++ src/main/java/com/example/demo/service/SmsService.java
@@ -1,9 +1,7 @@
 package com.example.demo.service;
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sns.AmazonSNSClient;
-import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sns.model.*;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
@@ -20,16 +18,24 @@ import java.util.Map;
 @Scope(value = "singleton")
 public final class SmsService {
 
-    public static void sendSms(String message, String phoneNumber) {
-        AmazonSNSClientBuilder snsClientBuilder = AmazonSNSClientBuilder.standard()
-                .withRegion(Regions.AP_SOUTH_1)
-                .withCredentials(new ProfileCredentialsProvider());
+    private static final String ACCESS_KEY = "AKIAJR7CLUD2W3I64G5Q";
+    private static final String SECRET_KEY = "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";
+    private static AmazonSNSClient snsClient;
 
-        AmazonSNSClient snsClient = (AmazonSNSClient) snsClientBuilder.build();
-        Map<String, MessageAttributeValue> smsAttributes = new HashMap<>();
-        //<set SMS attributes>
-//        setSmsAttributes(smsAttributes);
+    public SmsService() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
+        snsClient = new AmazonSNSClient(credentials);
         setDefaultSmsAttributes(snsClient);
+    }
+
+    public static void sendSms(String message, String phoneNumber) {
+//        AmazonSNSClientBuilder snsClientBuilder = AmazonSNSClientBuilder.standard()
+//                .withRegion(Regions.AP_SOUTH_1)
+//                .withCredentials(new ProfileCredentialsProvider());
+
+//        AmazonSNSClient snsClient = (AmazonSNSClient) snsClientBuilder.build();
+        Map<String, MessageAttributeValue> smsAttributes = new HashMap<>();
+//        setDefaultSmsAttributes(snsClient);
         sendSMSMessage(snsClient, message, phoneNumber, smsAttributes);
     }
 
@@ -42,23 +48,14 @@ public final class SmsService {
         System.out.println(result); // Prints the message ID.
     }
 
-//    private static void setSmsAttributes(Map<String, MessageAttributeValue> smsAttributes) {
-//        smsAttributes.put("AWS.SNS.SMS.SenderID", new MessageAttributeValue()
-//                .withStringValue("Network") //The sender ID shown on the device.
-//                .withDataType("String"));
-//        smsAttributes.put("AWS.SNS.SMS.MaxPrice", new MessageAttributeValue()
-//                .withStringValue("0.50") //Sets the max price to 0.50 USD.
-//                .withDataType("Number"));
-//        smsAttributes.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue()
-//                .withStringValue("Promotional") //Sets the type to promotional.
-//                .withDataType("String"));
-//    }
-
     private static void setDefaultSmsAttributes(AmazonSNSClient snsClient) {
         SetSMSAttributesRequest setRequest = new SetSMSAttributesRequest()
-                .addAttributesEntry("DeliveryStatusIAMRole", "arn:aws:iam::178519313808:role/role-sns")
                 .addAttributesEntry("DefaultSenderID", "Network")
-                .addAttributesEntry("DefaultSMSType", "Promotional");
+                .addAttributesEntry("MonthlySpendLimit", "1")
+                .addAttributesEntry("DeliveryStatusIAMRole", "arn:aws:iam::178519313808:role/role-sns")
+                .addAttributesEntry("DeliveryStatusSuccessSamplingRate", "10")
+                .addAttributesEntry("DefaultSMSType", "Promotional")
+                .addAttributesEntry("UsageReportS3Bucket", "network-app-bucket");
         snsClient.setSMSAttributes(setRequest);
         Map<String, String> myAttributes = snsClient.getSMSAttributes(new GetSMSAttributesRequest())
                 .getAttributes();
